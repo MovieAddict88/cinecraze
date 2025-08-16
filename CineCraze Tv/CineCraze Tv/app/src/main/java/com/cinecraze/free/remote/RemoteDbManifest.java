945d4ff5ac0cef2d0374d15038ab231fb6330526
MovieAddict88 <ronatorrejos1@gmail.com>
2025-08-17 06:40:47 +0800

    Delete CineCraze Tv/CineCraze Tv/app/src/main/java/com/cinecraze/free/remote directory

--- CineCraze Tv/CineCraze Tv/app/src/main/java/com/cinecraze/free/remote/RemoteDbManifest.java
+++
@@ -1,9 +0,0 @@
-package com.cinecraze.free.remote;
-
-public class RemoteDbManifest {
-    public String version;
-    public String dbUrl;
-    public long sizeBytes;
-    public String sha256; // checksum of the final unzipped DB file
-    public boolean zipped;
-}
